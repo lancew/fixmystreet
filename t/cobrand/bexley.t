openeducationaldata/ajeitaminharua

[Bexley] Show report ID in confirmation email.
As requested by the client, this adds the report ID to the report
confirmation email that is sent when an unregistered user creates a
report.

t/cobrand/bexley.t
@@ -5,6 +5,10 @@ use FixMyStreet::TestMech;
 use FixMyStreet::Script::Reports;
 use Catalyst::Test 'FixMyStreet::App';
 
+# disable info logs for this test run
+FixMyStreet::App->log->disable('info');
+END { FixMyStreet::App->log->enable('info'); }
+
 set_fixed_time('2019-10-16T17:00:00Z'); # Out of hours
 
 use_ok 'FixMyStreet::Cobrand::Bexley';
@@ -202,6 +206,22 @@ FixMyStreet::override_config {
         ], 'Request had multiple photos';
     };
 
+    subtest 'test ID in confirmation email' => sub {
+        $mech->clear_emails_ok;
+        $mech->log_out_ok;
+        $mech->get_ok('/report/new?latitude=51.455555&longitude=0.153560');
+        $mech->submit_form_ok({ with_fields => {
+            title => 'Test ID',
+            detail => 'Test ID',
+            category => 'Lamp post',
+            name => 'Normal User',
+            username => 'normal@example.org',
+        } });
+        my $report = FixMyStreet::DB->resultset('Problem')->search(undef, { rows => 1, order_by => { -desc => 'id' } })->single;
+        my $id = $report->id;
+        my $text = $mech->get_text_body_from_email;
+        like $text, qr/The report's reference number is $id/;
+    };
 };
 
 subtest 'nearest road returns correct road' => sub {

lancew/fixmystreet

t/cobrand/bexley.t
@@ -5,6 +5,10 @@ use FixMyStreet::TestMech;
 use FixMyStreet::Script::Reports;
 use Catalyst::Test 'FixMyStreet::App';
 
+# disable info logs for this test run
+FixMyStreet::App->log->disable('info');
+END { FixMyStreet::App->log->enable('info'); }
+
 set_fixed_time('2019-10-16T17:00:00Z'); # Out of hours
 
 use_ok 'FixMyStreet::Cobrand::Bexley';
@@ -212,6 +216,22 @@ FixMyStreet::override_config {
         $mech->content_contains('Posted anonymously by a non-staff user');
     };
 
+    subtest 'test ID in confirmation email' => sub {
+        $mech->clear_emails_ok;
+        $mech->log_out_ok;
+        $mech->get_ok('/report/new?latitude=51.455555&longitude=0.153560');
+        $mech->submit_form_ok({ with_fields => {
+            title => 'Test ID',
+            detail => 'Test ID',
+            category => 'Lamp post',
+            name => 'Normal User',
+            username_register => 'normal@example.org',
+        } });
+        my $report = FixMyStreet::DB->resultset('Problem')->search(undef, { rows => 1, order_by => { -desc => 'id' } })->single;
+        my $id = $report->id;
+        my $text = $mech->get_text_body_from_email;
+        like $text, qr/The report's reference number is $id/;
+    };
 };
 
 subtest 'nearest road returns correct road' => sub {